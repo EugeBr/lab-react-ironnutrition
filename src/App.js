ba47c3c4e63455044b0cd1d05f1b019841a1de14
EugeBr <eugebordenave@gmail.com>
2022-06-23 21:08:47 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import foods from './foods.json';
 import './App.css';
+import FoodBox from './components/FoodBox';
 
 function App() {
 
   const displayFoods = () => {
     return (
-      foods.map((food) => {
+      /*foods.map((food, index) => {
         return (
-          <div>
-            <p> {food.name} </p>
-            <img src= {food.image} width={100} />
-          </div>
+          <FoodBox
+          key={`food-${index}`}
+          {...food}
+          />
         )
-      })
+      })*/
+      <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
     )
   }
 
